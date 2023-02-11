84500437641c44fb8eb4cfd7327d0fb27046d3da
Will Carle <wcarle88@gmail.com>
2023-02-10 16:50:23 -0800

    Test build

---
+++ dist/src/test/mocks.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="jest" />
+import { SignalClient } from '../api/SignalClient';
+import RTCEngine from '../room/RTCEngine';
+declare const mocks: {
+    SignalClient: jest.MockedClass<typeof SignalClient>;
+    RTCEngine: jest.MockedClass<typeof RTCEngine>;
+    MockLocalVideoTrack: {
+        stop: jest.Mock<any, any, any>;
+    };
+};
+export default mocks;
+//# sourceMappingURL=mocks.d.ts.map
---
+++ dist/ts4.2/src/room/types.d.ts
@@ -0,0 +1,14 @@
+export type SimulationOptions = {
+    publish?: {
+        audio?: boolean;
+        video?: boolean;
+        useRealTracks?: boolean;
+    };
+    participants?: {
+        count?: number;
+        aspectRatios?: Array<number>;
+        audio?: boolean;
+        video?: boolean;
+    };
+};
+//# sourceMappingURL=types.d.ts.map
