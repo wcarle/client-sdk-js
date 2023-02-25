2be5f96da649535d913ec010c71eeffc1fe420c9
Will Carle <wcarle88@gmail.com>
2023-02-24 16:35:48 -0800

    test

--- dist/livekit-client.esm.mjs
+++ dist/livekit-client.esm.mjs
@@ -2168,59 +2168,52 @@ var utf8$2 = {};
   };
 })(utf8$2);
 
-var pool_1;
-var hasRequiredPool;
-function requirePool() {
-  if (hasRequiredPool) return pool_1;
-  hasRequiredPool = 1;
-  pool_1 = pool;
+var pool_1 = pool;
 
-  /**
-   * An allocator as used by {@link util.pool}.
-   * @typedef PoolAllocator
-   * @type {function}
-   * @param {number} size Buffer size
-   * @returns {Uint8Array} Buffer
-   */
+/**
+ * An allocator as used by {@link util.pool}.
+ * @typedef PoolAllocator
+ * @type {function}
+ * @param {number} size Buffer size
+ * @returns {Uint8Array} Buffer
+ */
 
-  /**
-   * A slicer as used by {@link util.pool}.
-   * @typedef PoolSlicer
-   * @type {function}
-   * @param {number} start Start offset
-   * @param {number} end End offset
-   * @returns {Uint8Array} Buffer slice
-   * @this {Uint8Array}
-   */
+/**
+ * A slicer as used by {@link util.pool}.
+ * @typedef PoolSlicer
+ * @type {function}
+ * @param {number} start Start offset
+ * @param {number} end End offset
+ * @returns {Uint8Array} Buffer slice
+ * @this {Uint8Array}
+ */
 
-  /**
-   * A general purpose buffer pool.
-   * @memberof util
-   * @function
-   * @param {PoolAllocator} alloc Allocator
-   * @param {PoolSlicer} slice Slicer
-   * @param {number} [size=8192] Slab size
-   * @returns {PoolAllocator} Pooled allocator
-   */
-  function pool(alloc, slice, size) {
-    var SIZE = size || 8192;
-    var MAX = SIZE >>> 1;
-    var slab = null;
-    var offset = SIZE;
-    return function pool_alloc(size) {
-      if (size < 1 || size > MAX) return alloc(size);
-      if (offset + size > SIZE) {
-        slab = alloc(SIZE);
-        offset = 0;
-      }
-      var buf = slice.call(slab, offset, offset += size);
-      if (offset & 7)
-        // align to 32 bit
-        offset = (offset | 7) + 1;
-      return buf;
-    };
-  }
-  return pool_1;
+/**
+ * A general purpose buffer pool.
+ * @memberof util
+ * @function
+ * @param {PoolAllocator} alloc Allocator
+ * @param {PoolSlicer} slice Slicer
+ * @param {number} [size=8192] Slab size
+ * @returns {PoolAllocator} Pooled allocator
+ */
+function pool(alloc, slice, size) {
+  var SIZE = size || 8192;
+  var MAX = SIZE >>> 1;
+  var slab = null;
+  var offset = SIZE;
+  return function pool_alloc(size) {
+    if (size < 1 || size > MAX) return alloc(size);
+    if (offset + size > SIZE) {
+      slab = alloc(SIZE);
+      offset = 0;
+    }
+    var buf = slice.call(slab, offset, offset += size);
+    if (offset & 7)
+      // align to 32 bit
+      offset = (offset | 7) + 1;
+    return buf;
+  };
 }
 
 var longbits;
@@ -2425,7 +2418,7 @@ function requireMinimal() {
     util.utf8 = utf8$2;
 
     // provides a node-like buffer pool in the browser
-    util.pool = requirePool();
+    util.pool = pool_1;
 
     // utility to work with the low and high bits of a 64 bit value
     util.LongBits = requireLongbits();
@@ -15784,6 +15777,7 @@ class RemoteTrack extends Track {
     super.enable();
   }
   stop() {
+    console.log("STOPPING TRACK ".concat(this.sid));
     this.stopMonitor();
     // use `enabled` of track to enable re-use of transceiver
     super.disable();
